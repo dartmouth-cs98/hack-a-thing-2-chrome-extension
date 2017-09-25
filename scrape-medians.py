c772d114bcb20ada9480a717b200b7392ebcd515
Barry <barry.a.yang.18@dartmouth.edu>
2017-09-25 15:20:35 -0400

    can get website

--- scrape-medians.py
+++ scrape-medians.py
@@ -1,20 +1,27 @@
 # import libraries
-import urllib2
+import urllib
 from bs4 import BeautifulSoup
 
 def scrape(url):
-    print(url)
+    r = urllib.urlopen(url).read()
+    soup = BeautifulSoup(r, "html.parser")
+    # print(soup.prettify()[0:1000])
+    for link in soup.find_all(align="center"):
+        print(link)
+
 
 if __name__ == "__main__":
 
-    print("Please input the webpage URL")
-    s = input('--> ')
+    scrape("http://www.dartmouth.edu/~reg/transcript/medians/17x.html")
 
-    while (s != 'quit' and s != 'q'):
-        try:
-            scrape(s)
-
-        except:                                   # anything else
-            print("Unexpected error")
-
-        s = input('--> ')
+    # print("Please input the webpage URL")
+    # s = raw_input('--> ')
+    #
+    # while (s != 'quit' and s != 'q'):
+    #     try:
+    #         scrape(s)
+    #
+    #     except:                                   # anything else
+    #         print("Unexpected error")
+    #
+    #     s = raw_input('--> ')
